905fa7f8e1735b4a68a7e0c37bdeb3911b2787f6
decamp <decamp@users.noreply.github.com>
2014-11-20 10:55:09 -0500

    Updated copyright headers.

--- src/test/java/bits/corrpoints/TransformFitterTest.java
+++ src/test/java/bits/corrpoints/TransformFitterTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2014. Philip DeCamp
+ * Released under the BSD 2-Clause License
+ * http://opensource.org/licenses/BSD-2-Clause
+ */
+
 package bits.corrpoints;
 
 import java.util.Random;
